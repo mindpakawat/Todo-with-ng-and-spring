0b6147ef1e7e17d09e187c6990e5a495ea168dc6
smilepakawat <smile.pakawat@odds.team>
2022-05-07 20:02:20 +0700

    Initial backend with spring boot

---
+++ back-end/src/main/java/com/pakawat/todo/backend/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.pakawat.todo.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public String helloWorldBean() {
+        return "Hello World";
+    }
+}
